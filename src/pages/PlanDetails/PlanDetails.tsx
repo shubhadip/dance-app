4f6845c811176c6cec41f3d373e6d54b5ea12b6d
Vivek Singh <viveksinghup.singh@gmail.com>
2021-06-21 22:21:56 +0530

    page ui added

--- src/pages/PlanDetails/PlanDetails.tsx
+++ src/pages/PlanDetails/PlanDetails.tsx
@@ -8,114 +8,123 @@ import timeIcon from "./../../assets/images/time.svg";
 export function PlanDetails() {
     return (
         <>
-            <div className="relative bg-gray-200 h-48 overflow-hidden">
-                <img
-                    src="https://picsum.photos/200/300"
-                    className="object-fill w-full"
-                />
-                <div className=" absolute left-0 top-0 w-full p-4 flex justify-between">
-                    <div className="w-8 flex justify-center items-center rounded-full h-8 bg-white">
-                        <img src={leftArrow} alt="back" />
-                    </div>
-                    <div className="w-8 flex justify-center items-center rounded-full h-8 bg-white">
-                        <img src={shareIcon} alt="share" />
-                    </div>
-                </div>
-            </div>
-            <div className="px-4">
-                <div className="w-full -mt-5 relative">
-                    <div className="w-10 h-10 rounded-full shadow-md bg-orange flex justify-center items-center">
-                        <img src={yoga} alt="yoga" />
+            <div className="pb-14">
+                <div className="relative bg-gray-200 h-48 overflow-hidden">
+                    <img
+                        src="https://picsum.photos/200/300"
+                        className="object-fill w-full"
+                    />
+                    <div className=" absolute left-0 top-0 w-full p-4 flex justify-between">
+                        <div className="w-8 flex justify-center items-center rounded-full h-8 bg-white">
+                            <img src={leftArrow} alt="back" />
+                        </div>
+                        <div className="w-8 flex justify-center items-center rounded-full h-8 bg-white">
+                            <img src={shareIcon} alt="share" />
+                        </div>
                     </div>
                 </div>
-                <div className="mt-2">
-                    <div className="text-xl font-bold">
+                <div className="px-4">
+                    <div className="w-full -mt-5 relative">
+                        <div className="w-10 h-10 rounded-full shadow-md bg-orange flex justify-center items-center">
+                            <img src={yoga} alt="yoga" />
+                        </div>
+                    </div>
+                    <div className="mt-2">
+                        <div className="text-xl font-bold">
+                            Live yoga with goga Live yoga with goga Live yoga
+                            with goga Live
+                        </div>
+                        <div className="text-xs mt-1 flex justify-between">
+                            <span className="opacity-50">gjhgj</span>
+                            <span className="text-orange">gjhgjgjgh</span>
+                        </div>
+                    </div>
+                </div>
+                <hr className="my-6" />
+                <div className="px-4">
+                    <div className="text-xl font-bold mb-3">
+                        Live yoga with goga Live yoga
+                    </div>
+                    <div className="text-sm opacity-50">
                         Live yoga with goga Live yoga with goga Live yoga with
-                        goga Live
+                        goga Live yoga with goga Live yoga with goga Live yoga
+                        with goga
                     </div>
-                    <div className="text-xs mt-1 flex justify-between">
-                        <span className="opacity-50">gjhgj</span>
-                        <span className="text-orange">gjhgjgjgh</span>
-                    </div>
-                </div>
-            </div>
-            <hr className="my-6" />
-            <div className="px-4">
-                <div className="text-xl font-bold mb-3">
-                    Live yoga with goga Live yoga
-                </div>
-                <div className="text-sm opacity-50">
-                    Live yoga with goga Live yoga with goga Live yoga with goga
-                    Live yoga with goga Live yoga with goga Live yoga with goga
-                </div>
-                <span className=" font-bold text-xs text-blue">READ MORE</span>
-            </div>
-            <hr className="my-6" />
-            <div className="px-4">
-                <div className="text-xl font-bold mb-3">Session Flow</div>
-                <div className="relative overflow-hidden ml-2 left-bar border-l-2 border-dashed">
-                    <div className="ml-8 mb-2">
-                        <div className="font-medium">Yoga with goga</div>
-                        <span className="opacity-50 text-xs">10 Minutes</span>
-                    </div>
-                    <div className="ml-8 mb-2">
-                        <div className="font-medium">Yoga with goga</div>
-                        <span className="opacity-50 text-xs">10 Minutes</span>
-                    </div>
-                </div>
-            </div>
-            <hr className="my-6" />
-            <div className="px-4 list-disc">
-                <div className="text-xl font-bold">Benefits</div>
-                <ul className="list-outside list-disc">
-                    <li className="ml-4 mt-2 opacity-50 text-15">
-                        goga Live yoga
-                    </li>
-                    <li className="ml-4 mt-2 opacity-50 text-15">
-                        goga Live yoga
-                    </li>
-                    <li className="ml-4 mt-2 opacity-50 text-15">
-                        goga Live yoga
-                    </li>
-                    <li className="ml-4 mt-2 opacity-50 text-15">
-                        goga Live yoga
-                    </li>
-                </ul>
-            </div>
-            <hr className="my-6" />
-            <div className="px-4 pb-8">
-                <div className="text-xl font-bold">How this works</div>
-                <div className="flex mt-3">
-                    <div className="w-5 mt-1 flex h-5 flex-shrink-0 mr-3">
-                        <img src={cameraIcon} alt="share" />
-                    </div>
-                    <span className="opacity-50">
-                        Live yoga with goga Live yoga with goga Live yoga with
-                        goga Live
+                    <span className=" font-bold text-xs text-blue">
+                        READ MORE
                     </span>
                 </div>
-                <div className="flex mt-3">
-                    <div className="w-5 mt-1 flex h-5 flex-shrink-0 mr-3">
-                        <img src={mailIcon} alt="share" />
+                <hr className="my-6" />
+                <div className="px-4">
+                    <div className="text-xl font-bold mb-3">Session Flow</div>
+                    <div className="relative overflow-hidden ml-2 left-bar border-l-2 border-dashed">
+                        <div className="ml-8 mb-2">
+                            <div className="font-medium">Yoga with goga</div>
+                            <span className="opacity-50 text-xs">
+                                10 Minutes
+                            </span>
+                        </div>
+                        <div className="ml-8 mb-2">
+                            <div className="font-medium">Yoga with goga</div>
+                            <span className="opacity-50 text-xs">
+                                10 Minutes
+                            </span>
+                        </div>
                     </div>
-                    <span className="opacity-50">
-                        Live yoga with goga Live yoga with goga Live yoga with
-                        goga Live
-                    </span>
                 </div>
-                <div className="flex mt-3">
-                    <div className="w-5 mt-1 flex h-5 flex-shrink-0 mr-3">
-                        <img src={timeIcon} alt="share" />
+                <hr className="my-6" />
+                <div className="px-4 list-disc">
+                    <div className="text-xl font-bold">Benefits</div>
+                    <ul className="list-outside list-disc">
+                        <li className="ml-4 mt-2 opacity-50 text-15">
+                            goga Live yoga
+                        </li>
+                        <li className="ml-4 mt-2 opacity-50 text-15">
+                            goga Live yoga
+                        </li>
+                        <li className="ml-4 mt-2 opacity-50 text-15">
+                            goga Live yoga
+                        </li>
+                        <li className="ml-4 mt-2 opacity-50 text-15">
+                            goga Live yoga
+                        </li>
+                    </ul>
+                </div>
+                <hr className="my-6" />
+                <div className="px-4 pb-8">
+                    <div className="text-xl font-bold">How this works</div>
+                    <div className="flex mt-3">
+                        <div className="w-5 mt-1 flex h-5 flex-shrink-0 mr-3">
+                            <img src={cameraIcon} alt="share" />
+                        </div>
+                        <span className="opacity-50">
+                            Live yoga with goga Live yoga with goga Live yoga
+                            with goga Live
+                        </span>
                     </div>
-                    <span className="opacity-50">
-                        Live yoga with goga Live yoga with goga Live yoga with
-                        goga Live
-                    </span>
+                    <div className="flex mt-3">
+                        <div className="w-5 mt-1 flex h-5 flex-shrink-0 mr-3">
+                            <img src={mailIcon} alt="share" />
+                        </div>
+                        <span className="opacity-50">
+                            Live yoga with goga Live yoga with goga Live yoga
+                            with goga Live
+                        </span>
+                    </div>
+                    <div className="flex mt-3">
+                        <div className="w-5 mt-1 flex h-5 flex-shrink-0 mr-3">
+                            <img src={timeIcon} alt="share" />
+                        </div>
+                        <span className="opacity-50">
+                            Live yoga with goga Live yoga with goga Live yoga
+                            with goga Live
+                        </span>
+                    </div>
                 </div>
+                <button className="text-15 fixed bottom-0 w-full left-0 font-bold text-white bg-green text-center py-5">
+                    Book Now for Rs.199
+                </button>
             </div>
-            <button className="text-15 font-bold w-full text-white bg-green text-center py-5">
-                Book Now for Rs.199
-            </button>
         </>
     );
 }
